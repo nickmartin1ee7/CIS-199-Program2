9e6a393bd45aee3ec7c69e5c89e746c956d0ba92
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-11-26 15:49:09 -0500

    Committing old repo

---
+++ Program2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.firstCharLabel = new System.Windows.Forms.Label();
+            this.firstCharTextBox = new System.Windows.Forms.TextBox();
+            this.creditHoursTextBox = new System.Windows.Forms.TextBox();
+            this.creditHoursLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultHeaderLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(130, 58);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(100, 23);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // firstCharLabel
+            // 
+            this.firstCharLabel.AutoSize = true;
+            this.firstCharLabel.Location = new System.Drawing.Point(9, 9);
+            this.firstCharLabel.Name = "firstCharLabel";
+            this.firstCharLabel.Size = new System.Drawing.Size(115, 13);
+            this.firstCharLabel.TabIndex = 1;
+            this.firstCharLabel.Text = "First letter of last name:";
+            // 
+            // firstCharTextBox
+            // 
+            this.firstCharTextBox.Location = new System.Drawing.Point(130, 6);
+            this.firstCharTextBox.MaxLength = 1;
+            this.firstCharTextBox.Name = "firstCharTextBox";
+            this.firstCharTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstCharTextBox.TabIndex = 0;
+            // 
+            // creditHoursTextBox
+            // 
+            this.creditHoursTextBox.Location = new System.Drawing.Point(130, 32);
+            this.creditHoursTextBox.Name = "creditHoursTextBox";
+            this.creditHoursTextBox.Size = new System.Drawing.Size(100, 20);
+            this.creditHoursTextBox.TabIndex = 1;
+            // 
+            // creditHoursLabel
+            // 
+            this.creditHoursLabel.AutoSize = true;
+            this.creditHoursLabel.Location = new System.Drawing.Point(32, 35);
+            this.creditHoursLabel.Name = "creditHoursLabel";
+            this.creditHoursLabel.Size = new System.Drawing.Size(92, 13);
+            this.creditHoursLabel.TabIndex = 3;
+            this.creditHoursLabel.Text = "Total credit hours:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoEllipsis = true;
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(12, 122);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(218, 30);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultHeaderLabel
+            // 
+            this.resultHeaderLabel.AutoSize = true;
+            this.resultHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultHeaderLabel.Location = new System.Drawing.Point(56, 106);
+            this.resultHeaderLabel.Name = "resultHeaderLabel";
+            this.resultHeaderLabel.Size = new System.Drawing.Size(130, 13);
+            this.resultHeaderLabel.TabIndex = 6;
+            this.resultHeaderLabel.Text = "First opportunity to register";
+            // 
+            // clearButton
+            // 
+            this.clearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.clearButton.Location = new System.Drawing.Point(24, 58);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.submitButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clearButton;
+            this.ClientSize = new System.Drawing.Size(242, 166);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultHeaderLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.creditHoursTextBox);
+            this.Controls.Add(this.creditHoursLabel);
+            this.Controls.Add(this.firstCharTextBox);
+            this.Controls.Add(this.firstCharLabel);
+            this.Controls.Add(this.submitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label firstCharLabel;
+        private System.Windows.Forms.TextBox firstCharTextBox;
+        private System.Windows.Forms.TextBox creditHoursTextBox;
+        private System.Windows.Forms.Label creditHoursLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label resultHeaderLabel;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Program2/Form1.cs
@@ -0,0 +1,121 @@
+﻿/**
+ *  Grading ID:  L8486
+ *  Class:       CIS 199-75
+ *  Program:     2
+ *  Due date:    10/17/19
+ *  Description: Determines the earliest time that a continuing UofL undergraduate student may register for Spring 2020 courses using the priority registration schedule available from the Registrar's site.
+ * **/
+
+using System;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Constants
+        private const int   SENIOR_CREDITS   = 90,  // Senior credit hour minimum limit
+                            JUNIOR_CREDITS   = 60,  // Junior credit hour minimum limit
+                            SOPHMORE_CREDITS = 30;  // Sophmore credit hour minimum limit
+        private const string HR_1 = "8:30",  // Eight-thirty time as string
+                             HR_2 = "10:00", // Ten time as string
+                             HR_3 = "11:30", // Eleven-thirty time as string
+                             HR_4 = "2:00",  // Two time as string
+                             HR_5 = "4:00";  // Four time as string
+        private const string DATE_1 = "MONDAY, NOV. 4",     // Senior registration date
+                             DATE_2 = "TUESDAY, NOV. 5",    // Junior registration date
+                             DATE_3 = "WEDNESDAY, NOV. 6",  // Early sophmore registration date
+                             DATE_4 = "THURSDAY, NOV. 7",   // Late sophmore registration date
+                             DATE_5 = "FRIDAY, NOV. 8",     // Early freshman registration date
+                             DATE_6 = "MONDAY, NOV. 11";    // Late freshman registration date
+
+        // Clear button resets text in both input textboxes and result label. Then selects first textbox.
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstCharTextBox.Text = "";
+            creditHoursTextBox.Text = "";
+            resultLabel.Text = "";
+            firstCharTextBox.Select();
+        }
+
+        // Variables
+        double credits;          // Total credit hours input
+        char lastName;           // First letter of last name input
+        string time;             // Time of registration
+        string date;             // Date of registration
+        bool altSetUsed = false; // Flag for indecating 'Time set 2' was used
+
+        // On submit button click: determine earliest time student may register for Spring 2020 classes by parsing input then using conditional if's to determine result label text.
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            altSetUsed = false;   // Reset flag on click event
+            // Parse and validate input
+            if (char.TryParse(firstCharTextBox.Text, out lastName) && char.IsLetter(lastName))
+            {
+                lastName = char.ToUpper(lastName);  // Format input to uppercase only
+                if (double.TryParse(creditHoursTextBox.Text, out credits) && credits >= 0)
+                {
+                    if (credits >= JUNIOR_CREDITS)
+                    {
+                        // Date set
+                        if (credits >= SENIOR_CREDITS)  { date = DATE_1; }
+                        else                            { date = DATE_2; }
+
+                        // Time set
+                        if      (lastName >= 'T') { time = HR_2; }
+                        else if (lastName >= 'P') { time = HR_1; } 
+                        else if (lastName >= 'J') { time = HR_5; }
+                        else if (lastName >= 'E') { time = HR_4; }
+                        else                      { time = HR_3; }
+                    }
+                    else // Else credits are either sophmore or freshman
+                    {
+                        // Date set
+                        if (credits >= SOPHMORE_CREDITS) { date = DATE_3; }
+                        else                             { date = DATE_5; }
+
+                        // Time set 1/2
+                        if      (lastName >= 'W')   { time = HR_4; }
+                        else if (lastName >= 'T')   { time = HR_3; }
+                        else if (lastName >= 'R')   { time = HR_2; }
+                        else if (lastName >= 'P')   { time = HR_1; }
+                        else if (lastName <= 'B')   { time = HR_5; }
+                        // Time set 2/2
+                        else
+                        {
+                            if      (lastName <= 'D')   { time = HR_1; }
+                            else if (lastName <= 'F')   { time = HR_2; }
+                            else if (lastName <= 'I')   { time = HR_3; }
+                            else if (lastName <= 'L')   { time = HR_4; }
+                            else                        { time = HR_5; }
+                            altSetUsed = true;
+                        }
+
+                        // If alternative set was used, use secondary dates for sophmore and freshmen
+                        if (altSetUsed == true)
+                        {
+                            if (credits >= SOPHMORE_CREDITS) { date = DATE_4; }
+                            else                             { date = DATE_6; }
+                        }
+                    }
+                    // Output
+                    resultLabel.Text = $"{date} @ {time}";
+                }
+                else // Int credits TryParse = false
+                {
+                    MessageBox.Show("Invalid entry in Total credit hours");
+                }
+            }
+            else // Char lastName TryParse = false
+            {
+                MessageBox.Show("Invalid entry in First character of Last Name");
+            }
+        }
+    }
+}
